8febc5f3e5787048b55060e4855caa93697f2577
hydra <1016590797@qq.com>
2018-01-14 19:48:57 +0800

    临时提交

---
+++ MacMetaphysics/MacMetaphysics/Formatter/TopTextFieldFormatter.h
@@ -0,0 +1,13 @@
+//
+//  TopTextFieldFormatter.h
+//  MacMetaphysics
+//
+//  Created by Hydra on 2018/1/1.
+//  Copyright © 2018年 毕志锋. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TopTextFieldFormatter : NSNumberFormatter
+
+@end
---
+++ MacMetaphysics/MacMetaphysics/Formatter/TopTextFieldFormatter.m
@@ -0,0 +1,13 @@
+//
+//  TopTextFieldFormatter.m
+//  MacMetaphysics
+//
+//  Created by Hydra on 2018/1/1.
+//  Copyright © 2018年 毕志锋. All rights reserved.
+//
+
+#import "TopTextFieldFormatter.h"
+
+@implementation TopTextFieldFormatter
+
+@end
